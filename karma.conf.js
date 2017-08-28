a58b5e65ff1627f41741b8411f679ee93a658e5d
K J Norton <rhchristian@gmail.com>
2017-08-28 01:57:38 +0100

    jjksdjksd

---
+++ karma.conf.js
@@ -0,0 +1,10 @@
+module.exports = function(config) {
+  config.set({
+    browsers: ['PhantomJS'],
+    frameworks: ['jasmine'],
+    files: [
+      'src/**/*.js',
+      'test/**/*.spec.js'
+    ]
+  });
+};
