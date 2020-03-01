75ec225884c570969ec63ce4641a15fa0745b4d6
K J Norton <rhchristian@gmail.com>
2020-03-01 00:58:39 +0000

    Update hello.anal.js

--- src/hello.anal.js
+++ src/hello.anal.js
@@ -11,8 +11,12 @@ describe('greeter', function () {
 // The releases page has links to download the standalone distribution, which contains everything you need to start running Jasmine. After downloading a particular version and unzipping, opening SpecRunner.html will run the included specs. You'll note that both the source files and their respective specs are linked in the <head> of the SpecRunner.html. To start using Jasmine, replace the source/spec files with your own.
 // Suites: describe Your Tests
 // A test suite begins with a call to the global Jasmine function describe with two parameters: a string and a function. The string is a name or title for a spec suite - usually what is being tested. The function is a block of code that implements the suite.
+
+
 // Specs
 // Specs are defined by calling the global Jasmine function it, which, like describe takes a string and a function. The string is the title of the spec and the function is the spec, or test. A spec contains one or more expectations that test the state of the code. An expectation in Jasmine is an assertion that is either true or false. A spec with all true expectations is a passing spec. A spec with one or more false expectations is a failing spec.
+
+
 
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
