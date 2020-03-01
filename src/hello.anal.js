888dfc3881cfd5261812a049f953cb3c120c925f
K J Norton <rhchristian@gmail.com>
2020-03-01 01:18:44 +0000

    Update hello.anal.js

--- src/hello.anal.js
+++ src/hello.anal.js
@@ -18,7 +18,7 @@ describe('greeter', function () {
 
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+    expect(false).toBe(true);
   });
 });
 
