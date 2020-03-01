eaf4ec72185e84f869c84c569900749f763e10cd
K J Norton <rhchristian@gmail.com>
2020-03-01 02:08:50 +0000

    Update hello.anal.js

--- src/hello.anal.js
+++ src/hello.anal.js
@@ -14,8 +14,6 @@ describe('greeter', function () {
 // Specs
 // Specs are defined by calling the global Jasmine function it, which, like describe takes a string and a function. The string is the title of the spec and the function is the spec, or test. A spec contains one or more expectations that test the state of the code. An expectation in Jasmine is an assertion that is either true or false. A spec with all true expectations is a passing spec. A spec with one or more false expectations is a failing spec.
 
-
-
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
